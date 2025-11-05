2267e2cd892b80bc08a8b3c56cf258e027018bbc
lihao <3585964164@qq.com>
2025-11-05 22:05:40 +0800

    Task300000000031 拍卖合约实现

---
+++ contracts/nftAuctino.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract nftAuction is ERC721{
+    struct Auction {
+        address seller;
+        uint256 startTime;
+        uint256 startPrice;
+        uint256 duration;
+        uint256 highestBid;
+        address highestBidder;
+        bool ended;
+        address nftAddress;
+        uint256 tokenId;
+    }
+
+    mapping(uint256 => Auction) public auctions;
+    address public admin;
+    uint256 public nextAuctionId;
+
+    constructor(){
+        admin = msg.sender;
+    }
+
+    function createAuction(uint256 _startPrice, uint256 _duration, address _nftAddress, uint256 _tokenId) public {
+        require(msg.sender == admin, "");
+        require(_startPrice > 0, "");
+        require(_duration >= 10, "");
+        auctions[nextAuctionId] = Auction({
+            seller: msg.sender,
+            startTime: block.timestamp,
+            startPrice: _startPrice,
+            duration: _duration,
+            highestBid: 0,
+            highestBidder: address(0),
+            ended: false,
+            nftAddress: _nftAddress,
+            tokenId: _tokenId
+        });
+        nextAuctionId++;
+    }
+
+    function auctionBid(uint256 _auctionId,uint256 price) external payable {
+        Auction auction = auctions[_auctionId];
+        require(!auction.ended && auction.startTime < block.timestamp < auction.startTime + auction.duration,"拍卖已结束或还未开始");
+        require(price >= auction.startPrice ,"出价不能小于起始价");
+        require(price < auction.highestBid,"出价小于该拍卖最高价");
+        if(auction.highestBid > 0){
+            payable(auction.highestBidder).transfer(auction.highestBid);
+        }
+        auction.highestBidder = msg.sender;
+        auction.highestBid = price;
+    }
+
+    function endAuction(uint256 _auctionId) external {
+        Auction auction = auctions[_auctionId];
+        require(!auction.ended,"拍卖已结束");
+        require(auction.startTime + auction.duration <= block.timestamp,"拍卖还未到结束时间");
+        IERC721(auction.nftAddress).safeTransferFrom(address(this), msg.sender, auction.tokenId);
+        auction.ended = true;
+    }
+}
